8986060de3b2af82b22e6eeb08918ba482b3f9a1
keth <keth@itu.dk>
2026-08-31 20:20:42 +0200

    Refactor main to use CsvHelper. Improves handling of data written and read from csv-files.

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,41 +1,47 @@
 ﻿using System.Globalization;
+using CsvHelper;
 
-#if FLAG_TEST
-    Console.WriteLine("omg my flag works");
-#endif
+public record Cheep(string Author, string Observation, long Timestamp);
 
-if(args.Length == 0){
-    Console.WriteLine("No argument was given");
-    return;
-} 
-if(args[0] == "read") read();
-else if (args[0] == "observe") observe(args[1]);
-
-static void read() 
+class Program
 {
-    string[] lines = File.ReadAllLines("bison_observe_cli_db.csv");
-    List<string> db = new List<string>();
-    foreach (string line in lines)
+    static void Main(string[] args)
     {
-        string[] parts = line.Split(',');
-        foreach (string part in parts)
+        #if FLAG_TEST
+            Console.WriteLine("omg my flag works");
+        #endif
+
+        if(args.Length == 0){
+            Console.WriteLine("No argument was given");
+            return;
+        }
+
+        if(args[0] == "read") read();
+        else if (args[0] == "observe") observe(args[1]);
+
+        static void read() 
         {
-            db.Add(part);
+            using var reader = new StreamReader("bison_observe_cli_db.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<Cheep>();
+            foreach (var record in records)
+            {
+                DateTimeOffset utcTime = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp);
+                Console.WriteLine($"{record.Author} @ {utcTime.LocalDateTime}: {record.Observation}");
+            }
+        }
+        
+        static void observe(string observation)
+        {
+            using var writer = new StreamWriter("bison_observe_cli_db.csv", append: true);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            string author = Environment.UserName;
+            long timeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            var cheep = new Cheep(author, observation, timeStamp);
+            csv.WriteRecord(cheep);
+            csv.NextRecord();
         }
     }
-    for (int i = 3; i < db.Count; i = i + 3)
-    {
-        long unixTime = long.Parse(db[i+2]);
-        DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unixTime).ToLocalTime();
-
-        Console.WriteLine(db[i] + " @ " + dto.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture) + " " + db[i+1]);
-    }
-}
-static void observe(string observation)
-{
-    string author = Environment.UserName;
-    long timeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-    string line = "\n" + author + ",\"" + observation + "\"," + timeStamp;
-    File.AppendAllText("bison_observe_cli_db.csv", line);
 }
