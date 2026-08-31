7207bc6975287a2aa282eec0cfb27b08d7dac9b4
atuf <atuf@itu.dk>
2026-08-31 17:26:45 +0200

    build script working with flags

--- Build.sh
+++ Build.sh
@@ -2,12 +2,14 @@
 
 release=0
 debug=0
+flags=()
 
-while getopts "rdc" opt; do
+while getopts "rdf:" opt; do
   case $opt in
     r) release=1 ;;
     d) debug=1 ;;
-    ?) echo "usage: $0 [-r] [-d]"; exit 1 ;;
+    f) flags+=("$OPTARG") ;;
+    ?) echo "usage: $0 [-r] [-d] [-f flag_name]"; exit 1 ;;
   esac
 done
 
@@ -16,5 +18,11 @@ if [[ $release -eq 0 && $debug -eq 0 ]]; then
   debug=1
 fi
 
-[[ $debug -eq 1 ]] && echo "building debug" && dotnet build Bison.CLI -c Debug
-[[ $release -eq 1 ]] && echo "building release" && dotnet build Bison.CLI -c Release
+IFS=';' dc="${flags[*]}"
+unset IFS
+
+flag_args=()
+[[ -n "$dc" ]] && flag_args=(-p:DefineConstants="$dc")
+
+[[ $debug -eq 1 ]] && echo "building debug" && dotnet build Bison.CLI -c Debug "${flag_args[@]}"
+[[ $release -eq 1 ]] && echo "building release" && dotnet build Bison.CLI -c Release "${flag_args[@]}"
