9011ce1a5229651e573dea7fb92a3ae1f055ea06
nole <nole@itu.dk>
2026-08-31 14:39:10 +0200

    read method
    The method parses a csv file, reads it and output the content.
    
    Co-authored-by: keth <keth@itu.dk>, atuf <atuf@itu.dk>, noga <noga@itu.dk>, badm <badm@itu.dk>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static void read() 
+{
+    string[] lines = File.ReadAllLines("bison_observe_cli_db.csv");
+    List<string> db = new List<string>();
+    foreach (string line in lines)
+    {
+        string[] parts = line.Split(',');
+        foreach (string part in parts)
+        {
+            db.Add(part);
+        }
+    }
+    for (int i = 3; i < db.Count; i = i + 3)
+    {
+        long unixTime = long.Parse(db[i+2]);
+        DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(unixTime).ToLocalTime();
+
+        Console.WriteLine(db[i] + " @ " + dto.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture) + " " + db[i+1]);
+    }
+}
