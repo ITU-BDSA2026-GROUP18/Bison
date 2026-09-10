2a3bb7ff02c50b4c7e588a8b8dc7843dd5d8e618
nole <nole@itu.dk>
2026-09-10 15:45:14 +0200

    csvDatabase to instance
    A csvDatabase object is now a static instance, which ensures only 1 database object is accessed in runtime

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -4,9 +4,17 @@ using CsvHelper;
 
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
+
+    private CSVDatabase() {}
+    private static CSVDatabase<T> instance = new();
+
+    public static CSVDatabase<T> getInstance()
+    {
+        return instance;
+    }
     public IEnumerable<T> read(int? limit = null)
     {
-        using var reader = new StreamReader("src/SimpleDB/bison_observe_cli_db.csv");
+        using var reader = new StreamReader("../SimpleDB/bison_observe_cli_db.csv");
         var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         var records = csv.GetRecords<T>().ToList();
         return records;
