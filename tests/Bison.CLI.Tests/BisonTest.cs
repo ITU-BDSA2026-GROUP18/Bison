1b1bd38d1eea285c3e159489e2fbd00614c4d474
badm2003 <badm@itu.dk>
2026-09-17 16:35:35 +0200

    very very aggressive path correction part 4

--- tests/Bison.CLI.Tests/BisonTest.cs
+++ tests/Bison.CLI.Tests/BisonTest.cs
@@ -6,11 +6,13 @@ namespace Bison.CLI.Tests;
 
 public class BisonTest
 {
+
+
 	public BisonTest()
 	{
 		//this should be a publicly accessible path normalization function, for now it isn't.
 		string temppath = Environment.CurrentDirectory;
-		int bisonIdx = temppath.IndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
+		int bisonIdx = temppath.LastIndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
 		Environment.CurrentDirectory = temppath.Substring(0, bisonIdx);
 
 		var db = CSVDatabase<Observation>.getInstance();
@@ -28,7 +30,7 @@ public class BisonTest
 			$"{expectedTime.LocalDateTime.ToString("d/M/yyyy HH:mm:ss", CultureInfo.InvariantCulture)}";
 
 		string temppath = Environment.CurrentDirectory;
-		int bisonIdx = temppath.IndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
+		int bisonIdx = temppath.LastIndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
 		Environment.CurrentDirectory = temppath.Substring(0, bisonIdx);
 
 		CSVDatabase<Observation>
@@ -56,7 +58,7 @@ public class BisonTest
 		// arrange
 		var sw = new StringWriter();
 		string temppath = Environment.CurrentDirectory;
-		int bisonIdx = temppath.IndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
+		int bisonIdx = temppath.LastIndexOf("bison", StringComparison.CurrentCultureIgnoreCase) + 5;
 		Environment.CurrentDirectory = temppath.Substring(0, bisonIdx);
 
 		CSVDatabase<Observation>
