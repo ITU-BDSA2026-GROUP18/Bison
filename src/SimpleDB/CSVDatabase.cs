6bf4f88260d020716faf4dab60e8de1cdd7bbca6
badm2003 <badm@itu.dk>
2026-09-18 17:08:16 +0200

    remove old path from simpledb

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -16,8 +16,6 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 		return instance;
 	}
 
-#if WEBSERVER
-
 	public IEnumerable<T> read(int? limit = null)
 	{
 		using var reader = new StreamReader(dbpath);
@@ -48,37 +46,4 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 		csv.NextRecord();
 		csv.WriteRecord(record);
 	}
-
-#else
-	public IEnumerable<T> read(int? limit = null)
-	{
-		using var reader = new StreamReader(dbpath);
-		var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-		var records = csv.GetRecords<T>().ToList();
-		return records;
-	}
-
-	public void store(T record)
-	{
-		using var writer = new StreamWriter(dbpath, append: true);
-		using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-
-		csv.NextRecord();
-		csv.WriteRecord(record);
-	}
-
-	public void setPath(string path) // set custom path, used for tests
-	{
-		dbpath = path;
-	}
-
-	public void storeNoAppend(T record) // will write to the database as if its empty, aka overwrite
-	{
-		using var writer = new StreamWriter(dbpath);
-		using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-		csv.WriteHeader<T>();
-		csv.NextRecord();
-		csv.WriteRecord(record);
-	}
-#endif
 }
